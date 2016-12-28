a292e90e09d8f4e8852cb82889ae2203a8e34f57
z956 <z789512346@gmail.com>
2016-12-28 21:39:56 +0800

    Set offset x and y or walk_animation

--- src/walk_animation.c
+++ src/walk_animation.c
@@ -5,16 +5,15 @@
 #include "sheet.h"
 
 static int walk_frames[] = {
-    LEFT_WALK1,
-    LEFT_WALK2,
-    LEFT_WALK1,
-    LEFT_WALK2,
-    LEFT_WALK1,
     RIGHT_WALK1,
     RIGHT_WALK2,
     RIGHT_WALK1,
     RIGHT_WALK2,
     RIGHT_WALK1,
+    LEFT_WALK1,
+    LEFT_WALK2,
+    LEFT_WALK1,
+    LEFT_WALK2,
     LEFT_WALK1,
 };
 static int walk_frames_count = sizeof(walk_frames) / sizeof(int);
@@ -26,10 +25,20 @@ void init_walk_animation() {
     walk_animation.cur = 0;
     walk_animation.frames = (struct frame_t*)malloc(sizeof(struct frame_t)*walk_frames_count);
 
+    int pivot = walk_frames_count / 2;
     for (int i = 0; i < walk_frames_count; i++) {
         walk_animation.frames[i].sheet = walk_frames[i];
-        walk_animation.frames[i].off_x = 0;
         walk_animation.frames[i].off_y = 0;
+
+        if (i == pivot) {
+            walk_animation.frames[i].off_x = pivot;
+        }
+        else if (i < pivot) {
+            walk_animation.frames[i].off_x = i;
+        }
+        else {
+            walk_animation.frames[i].off_x = walk_frames_count - i - 1;
+        }
     }
 }
 
